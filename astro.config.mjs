4413caf09747cc267e48ad92d722055220f0a740
vincentmungai <vinimunga@gmail.com>
2025-02-13 01:12:53 -0800

    A bunch of changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sanity from 'astro-sanity';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sanity({
+    projectId: '9bcnrig2',
+    dataset: 'production',
+    apiVersion: '2021-03-25',
+    useCdn: true,
+  })]
+});
