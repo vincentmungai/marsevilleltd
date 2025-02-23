e7de29237c2929a875585dd6422a6d4c84604ab4
vincentmungai <vinimunga@gmail.com>
2025-02-22 23:16:33 -0800

    Robots, google tags and site compression added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,28 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import sanity from 'astro-sanity';
+import compress from 'astro-compress';
+import sitemap from '@astrojs/sitemap';
+import viteCompression from 'vite-plugin-compression';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [sanity({
-    projectId: '9bcnrig2',
-    dataset: 'production',
-    apiVersion: '2021-03-25',
-    useCdn: true,
-  })]
+  integrations: [
+    sanity({
+      projectId: '9bcnrig2',
+      dataset: 'production',
+      apiVersion: '2021-03-25',
+      useCdn: true,
+    }),
+    compress(),
+    sitemap()
+  ],
+  vite: {
+    plugins: [
+      viteCompression({
+        algorithm: 'gzip',      // Options include 'gzip' and 'brotliCompress'
+        threshold: 10240,       // Only compress files larger than 10KB
+        deleteOriginFile: false // Keep original uncompressed files
+      }),
+    ],
+  },
 });
